b5048f46caf82a7695f5b36fc3f810507ae2d873
Thomas Caillet <129697125+rdmmf@users.noreply.github.com>
2024-11-05 19:30:57 +0100

    Test encryption image
    fichier TPs/chiffr_XOR.py

--- TPs/chiffr_XOR.py
+++ TPs/chiffr_XOR.py
@@ -3,26 +3,26 @@
 from bitarray import bitarray
 import os, random
 
+# Original message
+a = bitarray()
+filename =  "rond.png"
+with open("files/"+filename, "rb") as f:
+    a.fromfile(f)
+
+
 key = bitarray(0)
-input_key = input("Enter the key >")
+input_key = input("Enter the key for encryption >")
 key_size = len(input_key)*8
 
 key.frombytes(input_key.encode('utf-8'))
-print("KEY :",key.tobytes().decode('latin-1'))
 
-with open("key.bin", "wb") as f:
+with open("files/"+"key.bin", "wb") as f:
     key.tofile(f)
 
-with open("key.bin", "rb") as f:
+with open("files/"+"key.bin", "rb") as f:
     key = bitarray()
     key.fromfile(f)
 
-# Original message
-a = bitarray()
-message = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print("MESSAGE : ",message)
-# Encode message
-a.frombytes(message.encode('utf-8'))
 
 encoded = bitarray()
 for i in range(0, len(a)//key_size+1):
@@ -31,28 +31,33 @@ for i in range(0, len(a)//key_size+1):
         word += bitarray(key_size-len(word))
     
     encoded.extend(word ^ key)
-print(len(encoded))
 
-print("SAVE ENCODED MSG : ",encoded.to01())
 # Save in a file
-with open("test.bin", "wb") as f:
+with open("files/"+"encrypted_"+filename, "wb") as f:
     encoded.tofile(f)
 
+print("files/rond.png => files/encrypted_rond.png")
+
 key = bitarray(0)
-input_key = input("Enter the key >")
+input_key = input("Enter the key for decrypt. >")
 key_size = len(input_key)*8
 key.frombytes(input_key.encode('utf-8'))
 
 # Load from a file
 b = bitarray(0)
-with open("test.bin", "rb") as f:
+with open("files/"+"encrypted_"+filename, "rb") as f:
     b.fromfile(f)
 
 
-print("OPEN ENCODED MSG : ",b.to01())
 # Decode message
 decoded = bitarray()
 for i in range(0, len(b)//key_size):
     decoded.extend(b[i*key_size:(i+1)*key_size] ^ key)
 
-print("UNDECODED :",decoded.tobytes().decode('utf-8'))
+
+# Save in a file
+with open("files/"+"decoded_"+filename, "wb") as f:
+    decoded.tofile(f)
+
+
+print("files/encrypted_rond.png => files/decoded_rond.png")
