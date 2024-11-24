72e10f39357d7dde71f94804730b304dd575dcf7
Thomas Caillet <129697125+rdmmf@users.noreply.github.com>
2024-11-24 17:33:08 +0100

    Menu

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-def input_handler (options):
+import os, argparse
+
+def input_selection (options):
     # q une liste de choix
 
     for i, choice in enumerate(options):
         print(str(i+1)+") "+ choice)
-    print("q ou quit pour quitter")
-    
+    print("q) Quit")
     
     # Vérifier la validitré de l'entrée
     while True:
@@ -18,31 +19,101 @@ def input_handler (options):
         try:
             choice = int(choice_input)
         except:
-            print("Entrée invalide")
+            print("Invalid input")
             continue
 
         if choice < 1 or choice > len(options):
-            print("Option invalide")
+            print("Invalid input")
         else:
             return choice
         
+def login(name="", password=""):
+    while name == "":
+        name = input("Username >")
 
-def chiffrer():
-    print("Chiffrer un message")
+    # Check if the account exists
+    if not os.path.exists("accounts/"+name):
+        print("Account not found")
+        return
+    
+    while password == "":
+        password = input("Password >")
+    
+    # TEMPORAIRE
+    with open("accounts/"+name+"/password.txt", "r") as f:
+        saved_password = f.read()
 
-def dechiffrer():
-    print("Déchiffrer un message")
+    print("Logged in" if saved_password == password else "Wrong password")
+
+def create_account(name="", password=""):
+    while name == "":
+        name = input("Enter your username >")
+    while password == "":
+        password = input("Enter your password >")
+
+    # if no accounts directory, create it
+    if not os.path.exists("accounts"):
+        os.makedirs("accounts")
+    
+    # Create the account directory
+    if os.path.exists("accounts/"+name):
+        print("Account already exists")
+        return
+    
+    # Create the account
+    try: 
+        os.makedirs("accounts/"+name) 
+    except:
+        print("Error while creating the account")
+        return
+    
+    # Save the password
+    with open("accounts/"+name+"/password.txt", "w") as f:
+        f.write(password)
+
+    print("Account created")
+
+def main():
+    # Main menu loop
+    while True:
+        options = ["Login to an account", "Create an account"]
+        choice = input_selection(options)
+
+        if choice == 1:
+            login()
+        elif choice == 2:
+            create_account()
+        else:
+            print("Leaving...")
+            break
 
 if __name__ == "__main__":
 
-    while True:
-        options = ["Chiffrer un message", "Déchiffrer un message"]
-        choice = input_handler(options)
 
-        if choice == 1:
-            chiffrer()
-        elif choice == 2:
-            dechiffrer()
-        else:
-            print("Quitter...")
-            break
+    # CLI Arguments 
+    parser = argparse.ArgumentParser(description="Digital safe CLI tool")
+   
+    parser.add_argument("-l", "--login", help="Login username:password", required=False)
+    parser.add_argument("-c", "--create-account", help="Login username:password", required=False)
+
+    args = parser.parse_args()
+
+    if args.create_account:
+        try:
+            log_info = args.create_account.split(":")
+            create_account(log_info[0], log_info[1])
+        except:
+            print("Invalid username:password format")
+    if args.login:
+        try:
+            log_info = args.login.split(":")
+            login(log_info[0], log_info[1])
+        except:
+            print("Invalid username:password format")
+    else:
+        # If no arguments are passed, run the main loop
+        main()
+
+    
+
+    
